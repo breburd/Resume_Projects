8a7cd8329f0bc7d36e4c383999599870ff66fda3
breburd <99900904+breburd@users.noreply.github.com>
2022-04-19 14:59:11 -0400

    Add files via upload
    This Java project outputs the test cases of matrix addition and multiplication. This was one of my favorite projects because it focused on a lot of the math aspects of computer science and I was able to pull knowledge from Linear Algebra into this.

---
+++ Project1/src/project1/dimensionMismatch.java
@@ -0,0 +1,14 @@
+/*
+ * An exception for dimension mismatch.
+ * Bre Burd
+ */
+package project1;
+
+public class dimensionMismatch extends Exception
+{
+    public dimensionMismatch(String m)
+    {
+        super(m);
+    }
+    
+}
